5239b058deec56322b1ac2b258e618a7c7e421d1
LookassGit <lukaszszkolakonto@gmail.com>
2026-09-15 12:11:23 +0200

    dasd

--- app/src/main/java/com/example/kamilpoplawski4pt/MainActivity.java
+++ app/src/main/java/com/example/kamilpoplawski4pt/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -32,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         init();
+        ustawNumer();
+        wlaczanie();
     }
     private void init() {
         editText = findViewById(R.id.eTPralka);
@@ -45,10 +48,34 @@ public class MainActivity extends AppCompatActivity {
     private void ustawNumer() {
         zatwierdz.setOnClickListener(view -> {
             String sciagnietyTekst = editText.getText().toString();
-            int sciagnietyTekstParsed = Integer.parseInt(sciagnietyTekst);
+
+            String tekstDalej = sciagnietyTekst.replaceAll("[^0-9]", "");
+
+            if (tekstDalej.isEmpty()) {
+                numer.setText("Zle");
+                return;
+            }
+            int sciagnietyTekstParsed = Integer.parseInt(tekstDalej);
+
             if (sciagnietyTekstParsed >= 1 && sciagnietyTekstParsed <= 12) {
                 numer.setText("Numer prania: " + sciagnietyTekstParsed);
+            } else {
+                numer.setText("Zle");
             }
         });
     }
+    private void wlaczanie() {
+        wlacz.setOnClickListener(v -> {
+            isVacuumOn = !isVacuumOn;
+            if (isVacuumOn) {
+                wlacz.setText("Wyłącz");
+                odkurzac.setText("Odkurzacz włączony");
+            } else {
+                wlacz.setText("Włącz");
+                odkurzac.setText("Odkurzacz wyłączony");
+            }
+        });
+    }
+
+
 }
